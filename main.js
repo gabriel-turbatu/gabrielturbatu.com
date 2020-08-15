02800400caa39291c643104db267bbc70f107557
Gabriel Turbatu <gabriel.turbatu@primesourcing.ro>
2020-08-15 21:03:03 +0300

    bugfix unfade

--- main.js
+++ main.js
@@ -6,17 +6,18 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-list');
     const navElems = document.querySelectorAll('.nav-list li');
-
+    var isNavActive = false;
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
         
         navElems.forEach((link, index) => {
-            if(!link.style.animation)
+            if(!isNavActive)
                 link.style.animation = `navListFade 0.5s ease forwards ${index/5 + 0.5}s`
             else sleep(500).then(() => {
                 link.style.animation = '';
             });
         });
+        isNavActive = !isNavActive;
         burger.classList.toggle('activate');
     });
 }
